acd93075cd39c1ac9b28bafae1d8967d0381747a
Mist. GraphX <arnaud.berard@mister-graphx.com>
2019-04-05 16:40:33 +0200

    Add ability to remove sourcemap reference in a buffer stream, if no sourcemap prop exists

--- index.js
+++ index.js
@@ -9,11 +9,17 @@ module.exports = function () {
             cb(null, file);
             return;
         }
-
         // @TODO possibly throw if no sourcemap prop exists,
         // as it's no point using this plugin if there is no sourcemap
         if('sourceMap' in file) {
           delete file.sourceMap;
+        }else{
+          const regex = /\/\*# sourceMappingURL=.* \*\//gi;
+          const str = file.contents.toString() ;
+          const subst = `/**/`;
+          // The substituted value will be contained in the result variable
+          const content = str.replace(regex, subst);
+          file.contents = Buffer.from(content || '');
         }
 
         cb(null, file);
